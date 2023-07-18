0eec422e5daccabff57265083d42cf97861647d0
roopesh-kumar-sharma <roopesharma1335@gmail.com>
2023-07-18 09:04:26 +0545

    added userlist collection to the code

--- user_name.py
+++ user_name.py
@@ -1,15 +1,15 @@
-#This is  sample code for git practise
-users=[]
-print("Welcome To the College")
-user_continue='yes'
-
-name=input("Enter your name").lower
-print("Welcome to the College",name
-users=users.append(name)
-	
-if user_continue=='no':
-print("Thanks for the visit")
-print(users)
-
-
+print("Welcome to the College")
+student=[]
+student_visit="y"
+while student_visit=="y":
+    Fname=input("Enter the First name of Student : ").capitalize()
+    Lname = input("Enter the Last name of Student : ").capitalize()
+    Full_name=Fname + " " + Lname
+    student.append(Full_name)
+    student_visit=input("type y to continue and n to exit : ").lower()
+    if student_visit=="n":
+        print('')
+print("The names of students visiting collge")
+for name in student:
+    print(name)
 
